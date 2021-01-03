011b338e439dd8b2f190750071310ecfc92fca5f
mgbams <cmgbams@gmail.com>
2021-01-03 13:10:43 +0100

    modification of routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { RouterModule, Routes} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
 import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { AppRoutingModule } from './app-routing.module';
 
 const appRoutes: Routes = [
   {'path': 'list', component: ListEmployeesComponent},
